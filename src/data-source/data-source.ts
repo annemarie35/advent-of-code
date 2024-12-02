5a4750f1805cfc09c1316fde96380bc8d6b75825
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-02 10:15:20 +0100

    day 1 : map file read in a array of left and right list

--- src/data-source/data-source.ts
+++ src/data-source/data-source.ts
@@ -1,9 +1,17 @@
 import * as fs from 'node:fs'
 
 export const readFile = async (filePath: string) => {
+    let leftList: number[] = []
+    let rightList: number[] = []
+
     try {
         const data = fs.readFileSync(filePath, 'utf8')
-        return data
+        data.split(/\r?\n/).forEach((line) => {
+            const lineArray = line.replace('   ', ' ').split(' ')
+            leftList.push(Number(lineArray[0]))
+            rightList.push(Number(lineArray[1]))
+        })
+        return [leftList, rightList]
     } catch (err) {
         console.error(err)
     }
--- test/data-source/data-source.test.ts
+++ test/data-source/data-source.test.ts
@@ -5,11 +5,14 @@ dotenv.config()
 
 describe('Data Source', () => {
     describe('Read file', () => {
-        it('Should be able to read file', async () => {
+        it('Should be able to read file and return an array', async () => {
             const response = await readFile(
                 `${process.env.DIRNAME_PATH}advent-of-code-2024/test/data-source/text-day-1-test.txt`
             )
-            expect(response).toEqual('61967 56543')
+            expect(response).toEqual([
+                [61967, 11022],
+                [56543, 54992]
+            ])
         })
     })
 })
