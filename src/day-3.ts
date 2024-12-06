bb44549ab79332e1e98a9210a5d9b60ecff0be9c
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-06 15:53:54 +0100

    day 3 : solving second part

--- src/day-3.ts
+++ src/day-3.ts
@@ -1,16 +1,33 @@
-import { readFile, removeInvalidChars } from './data-source/data-source'
+import { readFile } from './data-source/data-source'
+
+function computeInstructions(validInstructions: IterableIterator<RegExpMatchArray>, total: number = 0) {
+    for (const validInstruction of validInstructions) {
+        total += Number(validInstruction[1]) * Number(validInstruction[2])
+    }
+    return total
+}
 
 export const solveDay3 = async (filePath: string) => {
-    const data = await readFile(filePath)
-    const cleanedData = removeInvalidChars(data)
     let total: number = 0
-    cleanedData.split('mul').forEach((line) => {
-        line.split(',')
-            .map((item) => Number(item))
-            .reduce((accumulator, currentValue) => {
-                total += accumulator * currentValue
-                return total
-            })
-    })
+    const corruptedCode = await readFile(filePath)
+
+    const validInstructions = corruptedCode.matchAll(/mul\((\d+),(\d+)\)/g)
+
+    total = computeInstructions(validInstructions, total)
+
+    return total
+}
+
+export const solveDay3SecondPart = async (filePath: string) => {
+    let total: number = 0
+    const corruptedCode = await readFile(filePath)
+
+    const enabledInstructions = corruptedCode
+        .split(/(?=do\(\)|don\'t\(\))/g)
+        .filter((tit) => !tit.startsWith("don't()"))
+    const validInstructions = enabledInstructions.toString().matchAll(/mul\((\d+),(\d+)\)/g)
+
+    total = computeInstructions(validInstructions, total)
+
     return total
 }
--- test/day-3.test.ts
+++ test/day-3.test.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import { solveDay3 } from '../src/day-3'
+import { solveDay3, solveDay3SecondPart } from '../src/day-3'
 
 dotenv.config()
 
@@ -10,5 +10,23 @@ describe('Compute results with valid instructions program', () => {
         )
 
         expect(result).toEqual(161)
+    })
+})
+
+describe('Compute results with valid instructions program', () => {
+    it('should return 48 as example given', async () => {
+        const result = await solveDay3SecondPart(
+            `${process.env.DIRNAME_PATH}advent-of-code-2024/test/data-source/text-day-3-2-test.txt`
+        )
+
+        expect(result).toEqual(48)
+    })
+
+    it('should return 26400432 as example given', async () => {
+        const result = await solveDay3SecondPart(
+            `${process.env.DIRNAME_PATH}advent-of-code-2024/test/data-source/text-day-3-2B-test.txt`
+        )
+
+        expect(result).toEqual(26400432)
     })
 })
