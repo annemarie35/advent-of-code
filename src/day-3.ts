a4aa4efde8e5ca80e489b12118b30d384e693d0b
Anne-Marie Esteves <anne-marie.esteves.ext@loccitane.com>
2024-12-13 17:09:24 +0100

    refacto : compute instructions function knows value of total buffer

--- src/day-3.ts
+++ src/day-3.ts
@@ -1,6 +1,7 @@
 import { readFile } from './data-source/data-source'
 
-function computeInstructions(validInstructions: IterableIterator<RegExpMatchArray>, total: number = 0) {
+function computeInstructions(validInstructions: IterableIterator<RegExpMatchArray>) {
+    let total: number = 0
     for (const validInstruction of validInstructions) {
         total += Number(validInstruction[1]) * Number(validInstruction[2])
     }
@@ -8,18 +9,14 @@ function computeInstructions(validInstructions: IterableIterator<RegExpMatchArra
 }
 
 export const solveDay3 = async (filePath: string) => {
-    let total: number = 0
     const corruptedCode = await readFile(filePath)
 
     const validInstructions = corruptedCode.matchAll(/mul\((\d+),(\d+)\)/g)
 
-    total = computeInstructions(validInstructions, total)
-
-    return total
+    return computeInstructions(validInstructions)
 }
 
 export const solveDay3SecondPart = async (filePath: string) => {
-    let total: number = 0
     const corruptedCode = await readFile(filePath)
 
     const enabledInstructions = corruptedCode
@@ -27,7 +24,5 @@ export const solveDay3SecondPart = async (filePath: string) => {
         .filter((instructions) => !instructions.startsWith("don't()"))
     const validInstructions = enabledInstructions.toString().matchAll(/mul\((\d+),(\d+)\)/g)
 
-    total = computeInstructions(validInstructions, total)
-
-    return total
+    return computeInstructions(validInstructions)
 }
